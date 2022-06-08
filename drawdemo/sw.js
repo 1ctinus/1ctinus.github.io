e4ff245d059ea11b7146aca6e95455334f05daa0
1ctinus <jtgc07@hotmail.com>
2022-06-08 17:12:09 -0500

    hopefully offline now

--- drawdemo/sw.js
+++ drawdemo/sw.js
@@ -1,7 +1,11 @@
-const cacheName = 'cache-v1.1'
+const cacheName = 'cache-v1.1.1'
 const resourcesToPrecache = [
-'index.html',
-'manifest.webmanifest'
+'./index.html',
+'./manifest.webmanifest',
+'./sw.js',
+'./apple.png',
+'./icon.png',
+'./tabicon.png'
 ]
 self.addEventListener('install', event => {
   console.log('Install event!')
@@ -9,8 +13,7 @@ self.addEventListener('install', event => {
     caches.open(cacheName)
       .then(cache => {
         return cache.addAll(resourcesToPrecache)
-       })
-   )
+       }))
 })
 self.addEventListener('activate', event => {
 console.log('Installa event!')
@@ -20,6 +23,8 @@ self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request)
     .then(cachedResponse => {
       return cachedResponse || fetch(event.request);
-    })
+    }).catch(function() {
+       return caches.match('./index.html');
+       })
   );
 })
